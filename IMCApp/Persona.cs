262f5621e3354ef5a183ad1c27e68293de5113e8
Sucus <Sucus@DESKTOP-C9S0PJ7>
2021-10-01 23:43:29 -0500

    Agregar archivos de proyecto.

---
+++ IMCApp/Persona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IMCApp
+{
+    public class Persona
+    {
+        public double Peso { get; set; }
+
+        public double Altura { get; set; }
+    }
+}
